11d55ec9e22d7da764303a7c6164ccf52e80978b
Mitrafan-Tor <dmitrytorzhikov@gmail.com>
2025-06-11 17:34:01 +0400

    изменения в pereval.urls.py

--- pereval/urls.py
+++ pereval/urls.py
@@ -3,4 +3,5 @@ from .views import SubmitDataAPI
 
 urlpatterns = [
     path('submitData/', SubmitDataAPI.as_view(), name='submit-data'),
+    path('submitData/<int:pk>/', SubmitDataAPI.as_view(), name='submit-data-detail'),
 ]
