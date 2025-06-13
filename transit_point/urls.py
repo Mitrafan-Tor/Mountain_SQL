f38d75d35a1b6e0c60e25787ea4a9af74dbae872
Mitrafan-Tor <dmitrytorzhikov@gmail.com>
2025-06-13 22:08:56 +0400

    изменения в главном urls.py

--- transit_point/urls.py
+++ transit_point/urls.py
@@ -1,6 +1,4 @@
-from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
@@ -10,17 +8,13 @@ schema_view = get_schema_view(
         title="Pereval API",
         default_version='v1',
         description="API для работы с перевалами",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@pereval.local"),
-    ),
-    public=True,
+        contact=openapi.Contact(email="contact@example.com"),
+    public=True,),
     permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='/swagger/', permanent=False)),  # Перенаправление на Swagger
-    path('admin/', admin.site.urls),
-    path('api/', include('pereval.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include('pereval.urls')),
 ]
