8f5ef61ac097455eceaa92db23f2911b0d343c43
Mitrafan-Tor <dmitrytorzhikov@gmail.com>
2025-06-20 12:47:47 +0400

    Update tests.py

--- pereval/tests.py
+++ pereval/tests.py
@@ -1,11 +1,9 @@
 from django.test import TestCase
-from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 from .models import User, Coords, Level, PerevalAdded, Image
 from .data_manager import PerevalManager
 import json
-import base64
 
 
 class PerevalManagerTest(TestCase):
